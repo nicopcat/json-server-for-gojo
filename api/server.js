600f3fd7819ee749194db1925c6eec6c0eb369af
Nic <tianfang07@outlook.com>
2023-07-19 15:19:05 +0800

    a

--- api/server.js
+++ api/server.js
@@ -3,9 +3,6 @@ const jsonServer = require('json-server')
 const server = jsonServer.create()
 const router = jsonServer.router('db.json')
 const middlewares = jsonServer.defaults()
-const fs = require('fs');
-const filePath = path.join("/tmp", "db.json");
-fs.writeFileSync(filePath, JSON.stringify(data));
 
 server.use(middlewares)
 // Add this before server.use(router)
